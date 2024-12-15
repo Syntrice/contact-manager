6fce5871e0f42d967a12c46e4aca7033ef4677f6
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-15 17:31:22 +0000

    Add project files.

---
+++ ContactManager.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ContactManager.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
