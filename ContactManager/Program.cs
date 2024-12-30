4be2a7f57552efb126f52732b4ac12f2c7ddfa22
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-30 15:05:40 +0000

    feat: Add ConsoleUIService
    Add new service for displaying a console user interface. The service is a
    BackgroundService so that it will be run on setup, and last for the lifecycle of
    the program. Program.cs has been adjusted to await for host.StartAsync and
    host.StopAsync so that it will exit once there are no services running.

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using ContactManager.Model;
 using ContactManager.Options;
-using ContactManager.Model;
 using ContactManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
@@ -10,24 +10,9 @@ HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection(DatabaseOptions.SectionName)); 
 builder.Services.AddDbContext<ContactDatabaseContext>();
 builder.Services.AddHostedService<DatabaseManagmentService>();
+builder.Services.AddHostedService<ConsoleUIService>();
 builder.Logging.ClearProviders();
 builder.Logging.AddDebug();
 IHost host = builder.Build();
-host.Run();
-
-//ContactDatabase.ApplyMigrations();
-
-//using (var db = new ContactDatabase())
-//{
-//    Console.WriteLine("Email Address Categories:");
-//    foreach (var category in db.EmailAddressCategories)
-//    {
-//        Console.WriteLine(category.Label);
-//    }
-
-//    Console.WriteLine("Phone Number Categories:");
-//    foreach (var category in db.PhoneNumberCategories)
-//    {
-//        Console.WriteLine(category.Label);
-//    }
-//}
+await host.StartAsync();
+await host.StopAsync();
---
+++ ContactManager/Services/ConsoleUIService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ContactManager.Services
+{
+    public class ConsoleUIService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("--- Contact Manager ---");
+            while (true)
+            {
+
+                Console.WriteLine("Enter a command:");
+                Console.WriteLine("Possible comamnds: 0 - Exit");
+                var command = Console.ReadLine();
+                switch (command)
+                {
+                    case "0":
+                        return Task.CompletedTask;
+                    default:
+                        Console.WriteLine("Unknown command");
+                        break;
+                }
+            }
+        }
+    }
+}
