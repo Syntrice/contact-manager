a333cb97f005d2a04b2bf175770933b1a90a5096
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-02 13:06:05 +0000

    feat: Add navigation properties to Contact

--- ContactManager/Data/Model/Contact.cs
+++ ContactManager/Data/Model/Contact.cs
@@ -10,5 +10,9 @@ namespace ContactManager.Data.Model
         [MaxLength(100)]
         public string Name { get; set; } = null!;
 
+        // Navigation Properties
+        public ICollection<PhoneNumber> PhoneNumbers { get; set; } = new List<PhoneNumber>();
+        public ICollection<EmailAddress> EmailAddresses { get; set; } = new List<EmailAddress>();
+
     }
 }
