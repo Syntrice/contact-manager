b26a0c86b8559800ba99c094665d029c9bb31bb8
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-02 12:23:04 +0000

    fix: Add ContactsDbContextFactory to fix migrations
    Add an implementation of IDesignTimeDbContextFactory to inject a default
    DatabaseOptions instance at design time. This allows migrations to work
    correctly again.

---
+++ ContactManager/Data/Model/ContactsDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using ContactManager.ApplicationOptions;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+
+namespace ContactManager.Data.Model
+{
+    public class ContactsDbContextFactory : IDesignTimeDbContextFactory<ContactsDbContext>
+    {
+        public ContactsDbContext CreateDbContext(string[] args)
+        {
+            DatabaseOptions options = new DatabaseOptions();
+            options.DatabasePath = "ContactManager.db";
+            IOptions<DatabaseOptions> databaseOptions = Options.Create(options);
+            return new ContactsDbContext(databaseOptions);
+        }
+    }
+}
